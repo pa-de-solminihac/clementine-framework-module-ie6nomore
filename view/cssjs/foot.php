f927673044fca48ef986f1a61cf053d6fe92f1ed
Pierre-Alexis de Solminihac <pa@quai13.com>
2013-06-11 15:38:23 +0200

    IE6NOMORE 2.2 compatibilite IE10 et ajouts CDATA

--- view/cssjs/foot.php
+++ view/cssjs/foot.php
@@ -7,7 +7,7 @@ if (!isset($data['ie6nomore_started'])) {
     // au document.ready : les elements externes ne sont pas encore charges, on n'a pas besoin de leurs dimensions
     jQuery(window).load(function() {
         // alerte modale si le navigateur est IE6
-        if (jQuery.browser.msie && jQuery.browser.version <= '7.') {
+        if (jQuery.browser.msie && jQuery.browser.version <= 7) {
             jQuery.fn.colorbox({
                 href: '<?php echo __WWW__ . '/ie6nomore'; ?>',
                 overlayClose: false,
